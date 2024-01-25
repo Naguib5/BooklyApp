f0593e157ff24a8980c4c93ccc6f8a2ac72a4856
Naguib5 <naguibnaguib2018@gmail.com>
2024-01-25 17:57:54 +0200

    change theme & add text to splash view body

---
+++ lib/Features/presentation/views/widget/splash_view_body.dart
@@ -0,0 +1,27 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Center(
+          child: Image.asset(
+            AssetsData.logo,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+        )
+      ],
+    );
+  }
+}
