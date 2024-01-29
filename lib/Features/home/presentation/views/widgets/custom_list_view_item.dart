199d1dbec0dcb92887d710a44434f46e488f208c
Naguib5 <naguibnaguib2018@gmail.com>
2024-01-29 05:35:07 +0200

    add featured list view to home screen

--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -5,22 +5,19 @@ class CustomListViewItem extends StatelessWidget {
   const CustomListViewItem({super.key});
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .27,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4, //  width size /height size
-        child: Container(
-          // height: 50,
-          // width: 50,
-          // height: MediaQuery.of(context).size.height * 0.3,
-          // width: MediaQuery.of(context).size.width * 0.3,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(16),
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsData.testImage),
-            ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4, //  width size /height size
+      child: Container(
+        // height: 50,
+        // width: 50,
+        // height: MediaQuery.of(context).size.height * 0.3,
+        // width: MediaQuery.of(context).size.width * 0.3,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testImage),
           ),
         ),
       ),
