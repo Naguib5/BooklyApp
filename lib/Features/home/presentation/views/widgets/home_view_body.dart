ee16e88c29930408ac0e11c87b6bb378726a1836
Naguib5 <naguibnaguib2018@gmail.com>
2024-02-07 01:10:22 +0200

    create app styles

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_par.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -8,19 +9,19 @@ class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomAppBar(),
-          FeaturedBookListView(),
-          SizedBox(
+          const CustomAppBar(),
+          const FeaturedBookListView(),
+          const SizedBox(
             height: 50,
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18.copyWith(fontFamily: kGTSectraFine),
           ),
           BestSellerListViewItem(),
         ],
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimeryColor = Color(0xff100E23);
 const kTransitionDuration = Duration(milliseconds: 250);
+const kGTSectraFine = 'GT-Sectra-Fine';
