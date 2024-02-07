9f80b001992525251339a549444738eda4a226cf
Naguib5 <naguibnaguib2018@gmail.com>
2024-02-07 04:34:39 +0200

    create book_details_view.dart

---
+++ lib/Features/home/presentation/views/book_detaiels_view.dart
@@ -0,0 +1,12 @@
+import 'package:bookly/Features/home/presentation/views/book_detaiels_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetaielsView extends StatelessWidget {
+  const BookDetaielsView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetaielsBody(),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:bookly/Features/home/presentation/views/widgets/best_seller_item_listview.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_par.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:bookly/core/utils/styles.dart';
