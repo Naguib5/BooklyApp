2dd398095174a9cf7783c728c2a5c38e78d7d75b
Naguib5 <naguibnaguib2018@gmail.com>
2024-03-20 17:47:24 +0200

    simileler book  also you like is warb with gester detector

--- lib/Features/home/presentation/views/widgets/books_action.dart
+++ lib/Features/home/presentation/views/widgets/books_action.dart
@@ -2,7 +2,6 @@ import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/core/utils/functions/launch_url.dart';
 import 'package:bookly/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class ActionButton extends StatelessWidget {
   const ActionButton({super.key, required this.bookModel});
--- lib/Features/home/presentation/views/widgets/simieler_books_listview.dart
+++ lib/Features/home/presentation/views/widgets/simieler_books_listview.dart
@@ -1,9 +1,11 @@
 import 'package:bookly/Features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/shimmer_widget/shimmer_featured_list_view.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/widgets/custom_progress_indecator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:shimmer/shimmer.dart';
 
 class SimilerBooksListview extends StatelessWidget {
@@ -19,13 +21,19 @@ class SimilerBooksListview extends StatelessWidget {
                 itemCount: state.books.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: ((context, index) {
-                  return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5),
-                      child: CustomBookImage(
-                        imageUrl: state.books[index].volumeInfo.imageLinks
-                                ?.thumbnail ??
-                            "",
-                      ));
+                  return GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).push(AppRouter.kbookdetaiels,
+                          extra: state.books[index]);
+                    },
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: CustomBookImage(
+                          imageUrl: state.books[index].volumeInfo.imageLinks
+                                  ?.thumbnail ??
+                              "",
+                        )),
+                  );
                 })),
           );
         } else if (state is SimilarBooksFailure) {
